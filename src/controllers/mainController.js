89557bbf28c8fb24e3215a09d696034c31998137
Patricia Bustos <pablictrad@gmail.com>
2024-07-10 07:15:43 -0300

    pasando datos del index a productDetail

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -7,7 +7,67 @@ const mainController = {
         const datos = {
             titulo: "COFFECODE"
         }
-        res.render('index',{'datos':datos});
+        const productos = [
+            {
+                id:1,
+                imagen: "/img/imagen_producto.jpg",
+                nombre: "Café Colombiano",
+                descripcion: "Café de alta calidad de Colombia.",
+                cantidad: 6,
+                categoria: "café"
+            },
+            {
+                id:2,
+                imagen: "/img/imagen-producto4.jpg",
+                nombre: "Café Brasileño",
+                descripcion: "Café robusto y fuerte de Brasil.",
+                cantidad: 6,
+                categoria: "café"
+            },
+            {
+                id:3,
+                imagen: "/img/imagen_producto.jpg",
+                nombre: "Café Etiopía",
+                descripcion: "Café suave y afrutado de Etiopía.",
+                cantidad: 6,
+                categoria: "café"
+            },
+            {
+                id:4,
+                imagen: "/img/imagen_producto.jpg",
+                nombre: "Café Kona",
+                descripcion: "Café exótico de Hawai.",
+                cantidad: 6,
+                categoria: "café"
+            },
+            {
+                id:5,
+                imagen: "/img/imagen_producto.jpg",
+                nombre: "Café de Sumatra",
+                descripcion: "Café con notas especiadas de Sumatra.",
+                cantidad: 6,
+                categoria: "café"
+            },
+            {
+                id:6,
+                imagen: "/img/imagen_producto.jpg",
+                nombre: "Café de Jamaica",
+                descripcion: "Café suave y aromático de Jamaica.",
+                cantidad: 6,
+                categoria: "café"
+            },
+            {
+                id:7,
+                imagen: "/img/imagen-producto3.jpg",
+                nombre: "Café de Guatemala",
+                descripcion: "Café con cuerpo completo de Guatemala.",
+                cantidad: 6,
+                categoria: "café"
+            }
+        ];
+        
+        
+        res.render('index',{'datos':datos,'listaProductos':productos});
     }
 }
 
--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -16,7 +16,76 @@ const productController = {
     },
     productDetail: (req, res) => {
         //res.sendFile(path.resolve(__dirname,'../views/products/productDetail.html'));
-        res.render('products/productDetail');
+        const id =req.params.id;
+        const productos = [
+         {
+             id: 1,
+             imagen: "/img/imagen_producto.jpg",
+             nombre: "Café Colombiano",
+             descripcion: "Café de alta calidad de Colombia.",
+             cantidad: 6,
+             categoria: "café",
+             precio: `$${(Math.random() * (50000 - 30000) + 30000).toFixed(2)}`
+         },
+         {
+             id: 2,
+             imagen: "/img/imagen-producto4.jpg",
+             nombre: "Café Brasileño",
+             descripcion: "Café robusto y fuerte de Brasil.",
+             cantidad: 6,
+             categoria: "café",
+             precio: `$${(Math.random() * (50000 - 30000) + 30000).toFixed(2)}`
+         },
+         {
+             id: 3,
+             imagen: "/img/imagen_producto.jpg",
+             nombre: "Café Etiopía",
+             descripcion: "Café suave y afrutado de Etiopía.",
+             cantidad: 6,
+             categoria: "café",
+             precio: `$${(Math.random() * (50000 - 30000) + 30000).toFixed(2)}`
+         },
+         {
+             id: 4,
+             imagen: "/img/imagen_producto.jpg",
+             nombre: "Café Kona",
+             descripcion: "Café exótico de Hawai.",
+             cantidad: 6,
+             categoria: "café",
+             precio: `$${(Math.random() * (50000 - 30000) + 30000).toFixed(2)}`
+         },
+         {
+             id: 5,
+             imagen: "/img/imagen_producto.jpg",
+             nombre: "Café de Sumatra",
+             descripcion: "Café con notas especiadas de Sumatra.",
+             cantidad: 6,
+             categoria: "café",
+             precio: `$${(Math.random() * (50000 - 30000) + 30000).toFixed(2)}`
+         },
+         {
+             id: 6,
+             imagen: "/img/imagen_producto.jpg",
+             nombre: "Café de Jamaica",
+             descripcion: "Café suave y aromático de Jamaica.",
+             cantidad: 6,
+             categoria: "café",
+             precio: `$${(Math.random() * (50000 - 30000) + 30000).toFixed(2)}`
+         },
+         {
+             id: 7,
+             imagen: "/img/imagen-producto3.jpg",
+             nombre: "Café de Guatemala",
+             descripcion: "Café con cuerpo completo de Guatemala.",
+             cantidad: 6,
+             categoria: "café",
+             precio: `$${(Math.random() * (50000 - 30000) + 30000).toFixed(2)}`
+         }
+     ];
+
+     const cafe = productos.find(e=>e.id==id);
+     //console.log(cafe);
+        res.render('products/productDetail',{'cafe':cafe});
     }
     
 }
