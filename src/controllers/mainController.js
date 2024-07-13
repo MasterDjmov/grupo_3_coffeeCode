8e9c1a7aa4e0c9bf558541b3c69fd6178aad7394
pablictrad <pablictrad@gmail.com>
2024-07-12 21:15:07 -0300

    creando products.json para simular BD

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,71 +1,13 @@
 const express = require('express');
 const path = require('path');
-
+const productos = require('../data/products.json');
 const mainController = {
     index: (req, res) => {
         //res.sendFile(path.resolve(__dirname,'../views/index.html'));
         const datos = {
-            titulo: "COFFECODE"
+            titulo: "CoffeCode-Inicio"
         }
-        const productos = [
-            {
-                id:1,
-                imagen: "/img/imagen_producto.jpg",
-                nombre: "Café Colombiano",
-                descripcion: "Café de alta calidad de Colombia.",
-                cantidad: 6,
-                categoria: "café"
-            },
-            {
-                id:2,
-                imagen: "/img/imagen-producto4.jpg",
-                nombre: "Café Brasileño",
-                descripcion: "Café robusto y fuerte de Brasil.",
-                cantidad: 6,
-                categoria: "café"
-            },
-            {
-                id:3,
-                imagen: "/img/imagen_producto.jpg",
-                nombre: "Café Etiopía",
-                descripcion: "Café suave y afrutado de Etiopía.",
-                descripcion2: "Café suave y afrutado de Etiopíahhhhhhhhhhhh.",
-                cantidad: 6,
-                categoria: "café"
-            },
-            {
-                id:4,
-                imagen: "/img/imagen_producto.jpg",
-                nombre: "Café Kona",
-                descripcion: "Café exótico de Hawai.",
-                cantidad: 6,
-                categoria: "café"
-            },
-            {
-                id:5,
-                imagen: "/img/imagen_producto.jpg",
-                nombre: "Café de Sumatra",
-                descripcion: "Café con notas especiadas de Sumatra.",
-                cantidad: 6,
-                categoria: "café"
-            },
-            {
-                id:6,
-                imagen: "/img/imagen_producto.jpg",
-                nombre: "Café de Jamaica",
-                descripcion: "Café suave y aromático de Jamaica.",
-                cantidad: 6,
-                categoria: "café"
-            },
-            {
-                id:7,
-                imagen: "/img/imagen-producto3.jpg",
-                nombre: "Café de Guatemala",
-                descripcion: "Café con cuerpo completo de Guatemala.",
-                cantidad: 6,
-                categoria: "café"
-            }
-        ];
+       
         
         
         res.render('index',{'datos':datos,'listaProductos':productos});
--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const productos = require('../data/products.json');
 
 const productController = {
     productCart: (req, res) => {
@@ -17,87 +18,7 @@ const productController = {
     productDetail: (req, res) => {
         //res.sendFile(path.resolve(__dirname,'../views/products/productDetail.html'));
         const id =req.params.id;
-        const productos = [
-         {
-             id: 1,
-             imagen: "/img/imagen_producto.jpg",
-             nombre: "Café Colombiano",
-             descripcion: "Café de alta calidad de Colombia.",
-             cantidad: 6,
-             categoria: "café",
-             precio: `$${(Math.random() * (50000 - 30000) + 30000).toFixed(2)}`,
-             pais: "Colombia",
-             productor: "ALIRIO GALINDEZ, FINCA VISTA HERMOSA"
-         },
-         {
-             id: 2,
-             imagen: "/img/imagen-producto4.jpg",
-             nombre: "Café Brasileño",
-             descripcion: "Café robusto y fuerte de Brasil.",
-             cantidad: 6,
-             categoria: "café",
-             precio: `$${(Math.random() * (50000 - 30000) + 30000).toFixed(2)}`,
-             pais: "Brasil",
-             productor: "FAZENDA SERTAO"
-         },
-         {
-             id: 3,
-             imagen: "/img/imagen_producto.jpg",
-             nombre: "Café Etiopía",
-             descripcion: "Café suave y afrutado de Etiopía.",
-             descripcion2: "Café suave y afrutado de Etiopíahhhhhhhhhhhh.",
-             cantidad: 6,
-             categoria: "café",
-             precio: `$${(Math.random() * (50000 - 30000) + 30000).toFixed(2)}`,
-             pais: "Etiopía",
-             productor: "VARIOS"
-         },
-         {
-             id: 4,
-             imagen: "/img/imagen_producto.jpg",
-             nombre: "Café Kona",
-             descripcion: "Café exótico de Hawai.",
-             cantidad: 6,
-             categoria: "café",
-             precio: `$${(Math.random() * (50000 - 30000) + 30000).toFixed(2)}`,
-             pais: "Hawai",
-             productor: "VARIOS"
-         },
-         {
-             id: 5,
-             imagen: "/img/imagen_producto.jpg",
-             nombre: "Café de Sumatra",
-             descripcion: "Café con notas especiadas de Sumatra.",
-             cantidad: 6,
-             categoria: "café",
-             precio: `$${(Math.random() * (50000 - 30000) + 30000).toFixed(2)}`,
-             pais: "Sumatra",
-             productor: "VARIOS"
-         },
-         {
-             id: 6,
-             imagen: "/img/imagen_producto.jpg",
-             nombre: "Café de Jamaica",
-             descripcion: "Café suave y aromático de Jamaica.",
-             cantidad: 6,
-             categoria: "café",
-             precio: `$${(Math.random() * (50000 - 30000) + 30000).toFixed(2)}`,
-             pais: "Jamaica",
-             productor: "VARIOS"
-         },
-         {
-             id: 7,
-             imagen: "/img/imagen-producto3.jpg",
-             nombre: "Café de Guatemala",
-             descripcion: "Café con cuerpo completo de Guatemala.",
-             cantidad: 6,
-             categoria: "café",
-             precio: `$${(Math.random() * (50000 - 30000) + 30000).toFixed(2)}`,
-             pais: "Guatemala",
-             productor: "IVÁN OVALLE. FINCA EL PEÑÓN"
-         }
-     ];
-
+       
      const cafe = productos.find(e=>e.id==id);
      //console.log(cafe);
         res.render('products/productDetail',{'cafe':cafe});
