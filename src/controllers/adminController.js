4db49b4f2e4b125e77558f11298d1e6d17d33de0
Patricia Bustos <pablictrad@gmail.com>
2024-07-16 19:32:43 -0300

    agregando rutas post y cartel de éxito

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -3,20 +3,33 @@ const path = require('path');
 const productos = require('../data/products.json');
 
 const adminController = {
+    formEditProduct: (req, res) => {
+        // res.sendFile(path.resolve(__dirname,'../views/admin/editProduct.html'));
+        const id =req.params.id;
+        const cafe = productos.find(e=>e.id==id);
+        console.log(cafe);
+        res.render('admin/editProduct',{'cafe':cafe,'msg':""});
+     },
     editProduct: (req, res) => {
        // res.sendFile(path.resolve(__dirname,'../views/admin/editProduct.html'));
+
        const id =req.params.id;
        const cafe = productos.find(e=>e.id==id);
-       console.log(cafe);
-       res.render('admin/editProduct',{'cafe':cafe});
+      
+       res.render('admin/editProduct',{'cafe':cafe, 'msg':"Edición Exitosa!!!"});
     },
     listProduct: (req, res) => {
        //res.sendFile(path.resolve(__dirname,'../views/admin/listProduct.html'));
        res.render('admin/listProduct');
     },
+    formRegisterProduct: (req, res) => {
+        //res.sendFile(path.resolve(__dirname,'../views/admin/registerProduct.html'));
+        res.render('admin/registerProduct',{'msg':""});
+    },
     registerProduct: (req, res) => {
         //res.sendFile(path.resolve(__dirname,'../views/admin/registerProduct.html'));
-        res.render('admin/registerProduct');
+        console.log("se inserto un producto");
+        res.render('admin/registerProduct',{'msg':"Producto Cargado Correctamente!!"});
     }
 }
 
--- src/routers/adminRouter.js
+++ src/routers/adminRouter.js
@@ -3,9 +3,11 @@ const adminController = require('../controllers/adminController');
 const express = require('express');
 const router = express.Router();
 
-router.get('/edit/:id', adminController.editProduct);
+router.get('/edit/:id', adminController.formEditProduct);// recibo los datos para editar
+router.post('/edit/:id', adminController.editProduct);
 router.get('/list', adminController.listProduct);
-router.get('/register', adminController.registerProduct);
+router.get('/register', adminController.formRegisterProduct); //cargo el formulario en blanco
+router.post('/register', adminController.registerProduct); // capturo lo que viene del formulario y lo inserto,
 
 
 module.exports = router;
