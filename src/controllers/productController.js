89e3d4312e6c90799cc207a6252e2460f04382a4
pablictrad <pablictrad@gmail.com>
2024-07-18 21:05:43 -0300

    agregando el mismo head en la vista del carrito

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -9,23 +9,36 @@ const productController = {
        res.render('products/productCart');
     },
     productCart_detalle: (req, res) => {
-       // res.sendFile(path.resolve(__dirname,'../views/products/productCart_detalle.html'));
-       res.render('products/productCart_detalle');
+      const id =req.params.id;
+      const msg = "";
+      const cafe = productos.find(e=>e.id==id);
+      if(req.session.user){
+             user= req.session.user
+       }else{
+             user="";
+       }
+       
+       res.render('products/productCart_detalle',
+         {
+            'cafe':cafe,
+            'user':user,
+            'msg':"Usted debe iniciar Sesión para Agregar al Carrito"
+         }
+       );
     },
     productCart_facturacion: (req, res) => {
        // res.sendFile(path.resolve(__dirname,'../views/products/productCart_facturacion.html'));
        res.render('products/productCart_facturacion');
     },
     productDetail: (req, res) => {
-        //res.sendFile(path.resolve(__dirname,'../views/products/productDetail.html'));
         const id =req.params.id;
         const msg = "";
         const cafe = productos.find(e=>e.id==id);
         if(req.session.user){
-         user= req.session.user
-     }else{
-         user="";
-     }
+               user= req.session.user
+         }else{
+               user="";
+         }
       
         res.render('products/productDetail',
          {
