99f5ae8ff4d5d92d4a0f9abe1e7bd40363ac3437
Patricia Bustos <pablictrad@gmail.com>
2024-08-06 18:11:38 -0300

    agregando rutas delete y put

--- src/routers/adminRouter.js
+++ src/routers/adminRouter.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/edit/:id', adminController.formEditProduct);// recibo los datos para editar
-router.post('/edit/:id', adminController.editProduct);
+router.put('/edit/:id', adminController.editProduct);
 // router.get('/list', adminController.listProduct);
 router.get('/create', adminController.formRegisterProduct); //cargo el formulario en blanco
 router.post('/create', adminController.registerProduct); // capturo lo que viene del formulario y lo inserto,
