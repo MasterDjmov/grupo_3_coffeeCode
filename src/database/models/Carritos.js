6e624ab3378e4f023a54552a68bfe055d5f29ee4
Loyola Leo Martin <masterdjmov@gmail.com>
2024-09-18 03:03:40 -0300

    subiendo relacion carritos->productos mediante su nm rel_carritos_productos

---
+++ src/database/models/Carritos.js
@@ -0,0 +1,62 @@
+module.exports = (Sequelize, DataTypes) => {
+    const Carritos = Sequelize.define('Carritos', {
+        id_carrito: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        id_usuario: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        id_estado: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        created_at: {
+            type: DataTypes.DATE,
+            allowNull: true
+        },
+        updated_at: {
+            type: DataTypes.DATE,
+            allowNull: true
+        },
+        total: {
+            type: DataTypes.DECIMAL(10, 2),
+            allowNull: true
+        },
+        id_medio_pago: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        id_forma_envio: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        carrito_id_carrito: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    }, {
+        tableName: 'carrito',
+        timestamps: true,
+        createdAt: 'created_at',
+        updatedAt: 'updated_at'
+    });
+
+    Carritos.associate = models => {
+        Carritos.belongsTo(models.Usuarios, {
+            as: 'usuario',
+            foreignKey: 'id_usuario'
+        });
+
+        Carritos.belongsToMany(models.Productos, {
+            as: 'productos',
+            through: 'rel_carrito_productos',
+            foreignKey: 'id_carrito',
+            otherKey: 'id_producto'
+        });
+    };
+    
+    return Carritos;
+};
