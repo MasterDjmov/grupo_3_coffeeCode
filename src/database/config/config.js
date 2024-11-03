6a2739d94e0f082d787098e55f6feb2beecb30df
Loyola Leo Martin <masterdjmov@gmail.com>
2024-11-03 16:52:29 -0300

    cambiando API por Api en controller user

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "",
+    "password": "djmov669",
     "database": "coffeecode",
     "host": "127.0.0.1",
     "dialect": "mysql"
