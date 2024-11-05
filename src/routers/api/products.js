19449c909f09b31b297128928f4473b36bb96f10
pablictrad <pablictrad@gmail.com>
2024-11-05 05:31:33 -0300

    corrigiendo ruta api/product

--- src/routers/api/products.js
+++ src/routers/api/products.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const tiposProductApiController = require('../../controllers/api/tiposProductApiController');
+const ProductApiController = require('../../controllers/api/productApiController');
 const router = express.Router();
 
 
-router.get('/', tiposProductApiController.lista); 
+router.get('/', ProductApiController.lista); 
 
 module.exports = router;
 
